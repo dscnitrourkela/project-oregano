731c1e6009991b42b212191c927bfb753505f5ed
CIPHERTron <pritish.samal918@gmail.com>
2021-06-02 23:59:07 +0530

    feat: Setup Sample Project Structure

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navbar, Layout, Footer } from '../components';
+
+const Homepage = () => {
+  return (
+    <div>
+      <Navbar />
+      <Layout />
+      <Footer />
+    </div>
+  );
+};
+
+export default Homepage;
