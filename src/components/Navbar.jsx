e33e952051a43f4c0b2022135e180f7523386d2a
CIPHERTron <pritish.samal918@gmail.com>
2021-07-13 13:32:14 +0530

    chore: Add Sample Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+import NavItems from './NavItems';
+
+const Container = styled.div`
+  ${tw`
+    bg-background-dark
+    py-7
+    flex
+    justify-around
+    text-white
+  `}
+`;
+
+const Heading = styled.h1`
+  ${tw`text-yellow-300 underline`}
+`;
 
 function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <Container>
+      <Heading>Brand</Heading>
+      <NavItems />
+      <h3>Sponsor</h3>
+    </Container>
   );
 }
 
