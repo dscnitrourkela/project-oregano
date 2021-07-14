55701e33d2f7404243619956073ec318923da189
Pritish Samal <pritish.samal918@gmail.com>
2021-07-15 00:43:50 +0530

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Layout() {
   return (
     <div>
-      <h1>Welcome to HackNITR 3.0. Have Fun! Hello!</h1>
+      <h1>Welcome to HackNITR 3.0. Have Fun! Hello! Heyy!!</h1>
     </div>
   );
 }
