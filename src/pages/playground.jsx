0e1abcfa5a17ff0727951cda12c7cbdc13da7921
Pritish Samal <pritish.samal918@gmail.com>
2021-07-17 13:11:54 +0530

    Update playground.jsx

--- src/pages/playground.jsx
+++ src/pages/playground.jsx
@@ -14,7 +14,7 @@ function playground() {
   return (
     <Container>
       <SectionContainer>
-        <Heading1>Heading 1</Heading1>
+        <Heading1>Heading 1</Headings1>
         <Heading2>Heading 2</Heading2>
         <Heading3>Heading 3</Heading3>
         <Heading4>Heading 4</Heading4>
