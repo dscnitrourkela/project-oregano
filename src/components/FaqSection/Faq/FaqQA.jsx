0e4693325761fd3b2e9f6355e571ba62898bd3db
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2021-07-20 07:14:06 +0530

    feat: Add FAQ Section

---
+++ src/components/FaqSection/Faq/FaqQA.jsx
@@ -0,0 +1,19 @@
+const STAGE = {
+    QUESA: "What is a Hackathon? How different is Online Hackathon?",
+    ANSA: "ANSa ANSa ANSa ANSa ANSa ANSa ANSa ANSa",
+    QUESB: "How to register?",
+    ANSB: "ANSb ANSb ANSb ANSb ANSb ANSb ANSb ANSb",
+    QUESC: " Who can participate?",
+    ANSC: "ANSc ANSc ANSc ANSc ANSc ANSc ANSc ANSc",
+    QUESD: "What if I haven’t been to a Hackathon before?",
+    ANSD: "ANSd ANSd ANSd ANSd ANSd ANSd ANSd ANSd",
+    QUESE: " What if I don't have a team or idea?",
+    ANSE: "ANSe ANSe ANSe ANSe ANSe ANSe ANSe ANSe",
+    QUESF: "Where can I find updates?",
+    ANSF: "ANSf ANSf ANSf ANSf ANSf ANSf ANSf ANSf",
+    QUESG:"  What could be the size of a team?",
+    ANSG: "ANSg ANSg ANSg ANSg ANSg ANSg ANSg ANSg"
+  };
+
+
+export default STAGE;
