e6780cfc268c8126ecb44ade5550bd25e84d7042
CIPHERTron <pritish.samal918@gmail.com>
2021-07-20 14:10:56 +0530

    fix: Update max-width in Tailwind Config & Add Breakpoints in Typography Elements

--- src/components/shared/Typography/Heading1.jsx
+++ src/components/shared/Typography/Heading1.jsx
@@ -4,7 +4,10 @@ import tw from 'twin.macro';
 export default styled.h1`
   ${tw`
         font-normal
-        text-5xl
+        xl:text-5xl
+        lg:text-4xl
+        md:text-3xl
+        sm:text-2xl
         text-color-primary
         font-roboto
     `}
--- tailwind.config.js
+++ tailwind.config.js
@@ -25,20 +25,20 @@ module.exports = {
       },
     },
     screens: {
-      sm: '640px',
-      // => @media (min-width: 640px) { ... }
+      '2xl': { max: '1535px' },
+      // => @media (max-width: 1535px) { ... }
 
-      md: '768px',
-      // => @media (min-width: 768px) { ... }
+      xl: { max: '1279px' },
+      // => @media (max-width: 1279px) { ... }
 
-      lg: '1024px',
-      // => @media (min-width: 1024px) { ... }
+      lg: { max: '1023px' },
+      // => @media (max-width: 1023px) { ... }
 
-      xl: '1280px',
-      // => @media (min-width: 1280px) { ... }
+      md: { max: '767px' },
+      // => @media (max-width: 767px) { ... }
 
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
+      sm: { max: '639px' },
+      // => @media (max-width: 639px) { ... }
     },
   },
   variants: {
