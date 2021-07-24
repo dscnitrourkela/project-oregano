f388465ad38b227be326136d6d3183de4af551b6
Ritesh Patil <riteshsp2000@gmail.com>
2021-07-24 15:49:41 +0530

    fix: resolve conflicts, cleanup code, export directly from components/index.js

--- src/components/HackerSection/BeAHacker.jsx
+++ src/components/HackerSection/BeAHacker.jsx
@@ -4,47 +4,40 @@ import tw from 'twin.macro';
 import { Container, SectionContainer } from '../shared';
 import Heading from '../shared/Typography/Heading2';
 import Body from '../shared/Typography/Body';
-import HackerCard from "./HackerCard";
+import HackerCard from './HackerCard';
 
 const HackerItems = styled.div`
-    ${tw`
+  ${tw`
         py-5
     `}
 `;
 
 const Description = styled.div`
-    ${tw`
+  ${tw`
         py-1
     `}
 `;
 
-
 function BeAHacker() {
-    return (
-        <SectionContainer>
-            <Container>
-            <HackerItems>
-                <Heading>
-                    How to be a hacker?
-                </Heading>
-                <Description>
-                    <Body>
-                        We have prepared a list of simple steps one needs to follow to become
-                    </Body>
-                    <Body>
-                        true hacker. The person who attains all these abilities will yield the power
-                    </Body>
-                    <Body>
-                        a to hack anything.
-                    </Body>
-                </Description>
-            </HackerItems>
-            <HackerItems>
-            <HackerCard />
-            </HackerItems>
-            </Container>
-        </SectionContainer>    
-    );
+  return (
+    <SectionContainer>
+      <Container>
+        <HackerItems>
+          <Heading>How to be a hacker?</Heading>
+          <Description>
+            <Body>We have prepared a list of simple steps one needs to follow to become</Body>
+            <Body>
+              true hacker. The person who attains all these abilities will yield the power
+            </Body>
+            <Body>a to hack anything.</Body>
+          </Description>
+        </HackerItems>
+        <HackerItems>
+          <HackerCard />
+        </HackerItems>
+      </Container>
+    </SectionContainer>
+  );
 }
 
-export default BeAHacker
+export default BeAHacker;
--- src/components/HackerSection/index.js
+++
@@ -1,3 +0,0 @@
-export { default as BeAHacker } from './BeAHacker';
-export { default as HackerCard } from './HackerCard';
-export { hackerConstants } from './hackerConstants';
