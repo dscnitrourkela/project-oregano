163ac2d7ae24e8146062f3e134c6e5501a30d80f
Ritesh Patil <riteshsp2000@gmail.com>
2021-07-24 16:02:42 +0530

    fix: resolve responsive grid errors

--- src/components/HackerSection/BeAHacker.jsx
+++ src/components/HackerSection/BeAHacker.jsx
@@ -1,41 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
-import { Container, SectionContainer } from '../shared';
+import { SectionContainer } from '../shared';
 import Heading from '../shared/Typography/Heading2';
 import Body from '../shared/Typography/Body';
 import HackerCard from './HackerCard';
 
 const HackerItems = styled.div`
   ${tw`
-        py-5
+      py-5
+      w-full
     `}
 `;
 
 const Description = styled.div`
   ${tw`
-        py-1
+      py-1
     `}
 `;
 
 function BeAHacker() {
   return (
     <SectionContainer>
-      <Container>
-        <HackerItems>
-          <Heading>How to be a hacker?</Heading>
-          <Description>
-            <Body>We have prepared a list of simple steps one needs to follow to become</Body>
-            <Body>
-              true hacker. The person who attains all these abilities will yield the power
-            </Body>
-            <Body>a to hack anything.</Body>
-          </Description>
-        </HackerItems>
-        <HackerItems>
-          <HackerCard />
-        </HackerItems>
-      </Container>
+      <HackerItems>
+        <Heading>How to be a hacker?</Heading>
+        <Description>
+          <Body>We have prepared a list of simple steps one needs to follow to become</Body>
+          <Body>true hacker. The person who attains all these abilities will yield the power</Body>
+          <Body>a to hack anything.</Body>
+        </Description>
+      </HackerItems>
+
+      <HackerCard />
     </SectionContainer>
   );
 }
