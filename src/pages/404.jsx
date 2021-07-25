85f65aeac2d3d54ca4150e63cc10fefa35dc1426
Ritesh Patil <riteshsp2000@gmail.com>
2021-07-25 19:25:03 +0530

    fix: resolve console errors, add manifest icons

---
+++ src/pages/404.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Components
+import { Layout, Heading1 } from '../components';
+
+const Error404 = () => (
+  <Layout>
+    <Heading1 style={{ width: '100%', textAlign: 'center', paddingTop: '10rem' }}>
+      404 Page Not Found
+    </Heading1>
+  </Layout>
+);
+
+export default Error404;
