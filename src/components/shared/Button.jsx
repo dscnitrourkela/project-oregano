4cd81647a939c328885dd450e2c48611bfab6ed9
CIPHERTron <pritish.samal918@gmail.com>
2021-07-30 13:27:54 +0530

    feat: Add Global Button Component

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,7 +1,25 @@
 import styled from 'styled-components';
 import tw from 'twin.macro';
 
-const Button = styled.button`
+const ButtonDark = styled.button`
+  ${tw`
+        h-14 
+        w-48
+        mt-6
+        mr-4
+        bg-white
+        border-0
+        rounded-full
+        cursor-pointer
+        bg-background-dark
+        text-color-primary
+        border-color-primary
+        border-2
+        sm:m-0
+    `}
+`;
+
+const ButtonLight = styled.button`
   ${tw`
         h-14 
         w-48
@@ -12,6 +30,9 @@ const Button = styled.button`
         rounded-full
         cursor-pointer
     `}
+  :hover {
+    background-color: #f8f0e3;
+  }
 `;
 
-export default Button;
+export { ButtonDark, ButtonLight };
