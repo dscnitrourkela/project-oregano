45e20b5f9d3a0baa78d39c777f6d6b0ba6c15920
CIPHERTron <pritish.samal918@gmail.com>
2021-07-31 17:00:55 +0530

    feat: Add Tracks Icons

--- config/content/Tracks.js
+++ config/content/Tracks.js
@@ -7,42 +7,38 @@ export default {
     {
       id: 'track-1',
       title: 'INCLUSIVITY',
-      img: {
-        src: 'https://res.cloudinary.com/dalqfvowk/image/upload/project-oregano/assets/ibjbhwktcjhgurc7dvnk.png',
-        alt: 'Track1: Inclusivity',
-      },
       content:
         'This track is a reminder of the beauty of inclusion and equality. Base your hacks on altering the social norms and build for the society on the lines of women empowerment, gender emancipation and societal justice.',
+      icon: 'https://res.cloudinary.com/dalqfvowk/image/upload/v1627730311/project-oregano/assets/yw1ew4euvqmuqpow2h6g.png',
+      alt: 'Inclusivity Icon',
+      hex: '#ebb722',
     },
     {
       id: 'track-2',
       title: 'COMMUNITY',
-      img: {
-        src: 'https://res.cloudinary.com/dalqfvowk/image/upload/project-oregano/assets/ibjbhwktcjhgurc7dvnk.png',
-        alt: 'Track2: Community',
-      },
       content:
         'This track serves to build for the community. Build for solving the problems of the society. Find solutions for the problems in healthcare, education etc. Each and every project that makes our lives better will adore this track.',
+      icon: 'https://res.cloudinary.com/dalqfvowk/image/upload/v1627730311/project-oregano/assets/pwcokehizmndjmltve3f.png',
+      alt: 'Community Icon',
+      hex: '#71a0ce',
     },
     {
       id: 'track-3',
       title: 'SUSTAINABILITY',
-      img: {
-        src: 'https://res.cloudinary.com/dalqfvowk/image/upload/project-oregano/assets/ibjbhwktcjhgurc7dvnk.png',
-        alt: 'Track3: Sustainability',
-      },
       content:
         'This track will seek to build projects that will make our lives more sustainable, including but not limited to teaching others how to ethically consume and creating greener technologies for the future.',
+      icon: 'https://res.cloudinary.com/dalqfvowk/image/upload/v1627730311/project-oregano/assets/udxogwtrkzphm22s6aek.png',
+      alt: 'Sustainability Icon',
+      hex: '#e78967',
     },
     {
       id: 'track-4',
       title: 'OPEN INNOVITY',
-      img: {
-        src: 'https://res.cloudinary.com/dalqfvowk/image/upload/project-oregano/assets/ibjbhwktcjhgurc7dvnk.png',
-        alt: 'Track4: Open Innovity',
-      },
       content:
         "This track demands creativity. Don't feel afraid to think out of the box and bend the rules. Be innovative by taking inspiration from existing ideas and build something unique that aligns with your ideas. Like we did to the track name!",
+      icon: 'https://res.cloudinary.com/dalqfvowk/image/upload/v1627730311/project-oregano/assets/c6wx3wso2fwzlk1gpdny.png',
+      alt: 'Open Innovity Icon',
+      hex: '#8fa963',
     },
   ],
 };
