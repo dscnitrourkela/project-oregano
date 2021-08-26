ff7d924f59a10dcf0adf6384fbb0022617c0d928
Ritesh Patil <riteshsp2000@gmail.com>
2021-08-26 20:00:36 +0530

    fix: minor spacing changes

--- src/components/HeroSection/HeroContent.jsx
+++ src/components/HeroSection/HeroContent.jsx
@@ -16,7 +16,7 @@ import DevfolioButton from '../shared/DevfolioButton';
 
 // Assets
 import { hero } from '../../../config/content';
-import config from '../../../config/website';
+// import config from '../../../config/website';
 
 const ContentContainer = styled.div`
   ${tw`
@@ -62,33 +62,33 @@ const Slogan = styled(HighlightBody)`
   `}
 `;
 
-const SponsorInvite = styled.div`
-  ${tw`my-6
-  gap-1
-  inline-flex
-  lg:block
-  md:inline-flex
-  `}
-`;
+// const SponsorInvite = styled.div`
+//   ${tw`my-6
+//   gap-1
+//   inline-flex
+//   lg:block
+//   md:inline-flex
+//   `}
+// `;
 
 // const InlineWrapper = styled.div`
 //   ${tw`flex mr-10 sm:mr-0 sm:justify-between sm:items-center sm:mt-4`}
 // `;
 
-const HighlightLink = styled.a`
-  ${tw`
-  font-normal
-  text-base
-  2xl:text-base
-  lg:text-base
-  md:text-base
-  sm:text-sm
-  text-color-primary
-  font-roboto
-  no-underline
-  ml-2
-  `}
-`;
+// const HighlightLink = styled.a`
+//   ${tw`
+//   font-normal
+//   text-base
+//   2xl:text-base
+//   lg:text-base
+//   md:text-base
+//   sm:text-sm
+//   text-color-primary
+//   font-roboto
+//   no-underline
+//   ml-2
+//   `}
+// `;
 
 // const NonStyledLink = styled.a`
 //   ${tw`
@@ -111,12 +111,12 @@ function HeroContent() {
           <ButtonDark>Join Discord</ButtonDark>
         </NonStyledLink>
       </InlineWrapper> */}
-      <SponsorInvite>
+      {/* <SponsorInvite>
         <Body>Want to join the HackNITR family? </Body>
         <HighlightLink href={config.join} target='_blank' rel='noreferrer'>
           Join Us
         </HighlightLink>
-      </SponsorInvite>
+      </SponsorInvite> */}
     </ContentContainer>
   );
 }
