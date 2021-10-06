8fd0a0879c0e4dc21d194e3d0422e15e94e690f7
Ritesh Patil <riteshsp2000@gmail.com>
2021-10-06 21:22:41 +0530

    feat: create prizes navbar

--- src/components/Prizes/Main.jsx
+++ src/components/Prizes/Main.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Libraries
 import styled from 'styled-components';
 
 // Components
 import { SectionLayout } from '../shared';
+import Navbar from './Navbar';
 import Overall from './Overall';
 
 const MainContainer = styled.div`
@@ -14,24 +15,49 @@ const MainContainer = styled.div`
   border-radius: 16px;
   overflow: hidden;
   padding: 40px 50px;
+  padding-top: 20px;
   margin-top: 20px;
 `;
 
-// const STAGES = {
-//   OVERALL: 'Overall Prizes',
-//   TRACKS: 'Track Prizes',
-//   SPONSORED: 'Sponsored Track Prizes',
-//   MLH: 'MLH Goodies',
-// };
+const STAGES = {
+  OVERALL: 'Overall Prizes',
+  TRACKS: 'Track Prizes',
+  SPONSORED: 'Sponsored Track Prizes',
+  MLH: 'MLH Goodies',
+};
+
 // eslint-disable-next-line arrow-body-style
 const BeAHacker = () => {
-  // const [state, setStage] = useState(STAGES.OVERALL);
+  const [stage, setStage] = useState(STAGES.OVERALL);
 
   // Stage Handlers
-  // const setStageToOverall = setStage(STAGES.OVERALL);
-  // const setStageToTracks = setStage(STAGES.TRACKS);
-  // const setStageToSponsored = setStage(STAGES.SPONSORED);
-  // const setStageToMLH = setStage(STAGES.MLH);
+  const setStageToOverall = () => setStage(STAGES.OVERALL);
+  const setStageToTracks = () => setStage(STAGES.TRACKS);
+  const setStageToSponsored = () => setStage(STAGES.SPONSORED);
+  const setStageToMLH = () => setStage(STAGES.MLH);
+
+  const navItems = [
+    {
+      title: 'Overall',
+      setStage: setStageToOverall,
+      key: STAGES.OVERALL,
+    },
+    {
+      title: 'Tracks',
+      setStage: setStageToTracks,
+      key: STAGES.TRACKS,
+    },
+    {
+      title: 'Sponsored Tracks',
+      setStage: setStageToSponsored,
+      key: STAGES.SPONSORED,
+    },
+    {
+      title: 'MLH Goodies',
+      setStage: setStageToMLH,
+      key: STAGES.MLH,
+    },
+  ];
 
   return (
     <SectionLayout
@@ -39,6 +65,7 @@ const BeAHacker = () => {
       description='Click on the tabs to see the various prizes'
     >
       <MainContainer>
+        <Navbar navItems={navItems} stage={stage} />
         <Overall />
       </MainContainer>
     </SectionLayout>
