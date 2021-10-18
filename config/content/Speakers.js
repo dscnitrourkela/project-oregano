eb758be038ef89a7e15bf9fb8af13230fb65fcb7
Sonakshi1901 <sonakshi1901@gmail.com>
2021-10-18 14:05:40 +0400

    add: content file for speakers

---
+++ config/content/Speakers.js
@@ -0,0 +1,64 @@
+/* eslint-disable max-len */
+export default {
+    title: 'Meet our Speakers',
+    content:
+      'We have prepared a list of simple steps one needs to follow to become a true hacker. The person who attains all these abilities will yield the power to hack anything.',
+    speakers: [
+      {
+        id: 'speaker-1',
+        name: 'sheldon lee cooper',
+        designation: 'Head of Design @Microsoft',
+        content:
+          'Reinventing Skeumorphism for the modern UI',
+        alt: 'speaker: sheldon lee cooper',
+        img: {
+          src: ' ',
+          width: '100%',
+        },
+        date: '29th October 2021',
+        time: '6:00pm IST'
+      },
+      {
+        id: 'speaker-2',
+        name: 'sheldon lee cooper',
+        designation: 'Head of Design @Microsoft',
+        content:
+          'Reinventing Skeumorphism for the modern UI',
+        alt: 'speaker: sheldon lee cooper',
+        img: {
+          src: ' ',
+          width: '100%',
+        },
+        date: '29th October 2021',
+        time: '6:00pm IST'
+      },
+      {
+        id: 'speaker-3',
+        name: 'sheldon lee cooper',
+        designation: 'Head of Design @Microsoft',
+        content:
+          'Reinventing Skeumorphism for the modern UI',
+        alt: 'speaker: sheldon lee cooper',
+        img: {
+          src: ' ',
+          width: '100%',
+        },
+        date: '29th October 2021',
+        time: '6:00pm IST'
+      },
+      {
+        id: 'speaker-4',
+        name: 'sheldon lee cooper',
+        designation: 'Head of Design @Microsoft',
+        content:
+          'Reinventing Skeumorphism for the modern UI',
+        alt: 'speaker: sheldon lee cooper',
+        img: {
+          src: ' ',
+          width: '100%',
+        },
+        date: '29th October 2021',
+        time: '6:00pm IST'
+      }
+    ]
+}
