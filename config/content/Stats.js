d2df136b03b5db401853b8a983b8169b25e7f525
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2021-10-21 13:06:17 +0530

    update cloudinary links

--- config/content/Stats.js
+++ config/content/Stats.js
@@ -4,14 +4,14 @@ export default {
   content:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hac tempus quam in velit in varius purus. Lacus suscipit lacus, risus libero pretium tellus et et lacinia. Tortor odio id aliquet nec.',
   statsMobile: {
-    icon: 'https://res.cloudinary.com/dscnitrourkela/image/upload/project-oregano/assets/z2rskuruxj2za4zgt4oj.png',
+    icon: 'https://res.cloudinary.com/riteshsp2000/image/upload/project-oregano/assets/Mockup_e5fuxn.png',
     icon2:
       'https://res.cloudinary.com/dscnitrourkela/image/upload/project-oregano/assets/v7hlmjra9fs024e0u1xz.png',
   },
   winners: [
     {
       id: 1,
-      img: 'https://res.cloudinary.com/dscnitrourkela/image/upload/project-oregano/assets/v7hlmjra9fs024e0u1xz.png',
+      img: 'https://res.cloudinary.com/riteshsp2000/image/upload/project-oregano/assets/Ellipse_41_1_hopvdw.png',
       name: 'Name 1',
       competition: 'Name of Competition',
       rank: 'Rank : 1st',
@@ -19,7 +19,7 @@ export default {
     },
     {
       id: 2,
-      img: 'https://res.cloudinary.com/dscnitrourkela/image/upload/project-oregano/assets/v7hlmjra9fs024e0u1xz.png',
+      img: 'https://res.cloudinary.com/riteshsp2000/image/upload/project-oregano/assets/Ellipse_41_1_hopvdw.png',
       name: 'Name 2',
       competition: 'Name of Competition',
       rank: 'Rank : 1st',
@@ -27,7 +27,7 @@ export default {
     },
     {
       id: 3,
-      img: 'https://res.cloudinary.com/dscnitrourkela/image/upload/project-oregano/assets/v7hlmjra9fs024e0u1xz.png',
+      img: 'https://res.cloudinary.com/riteshsp2000/image/upload/project-oregano/assets/Ellipse_41_1_hopvdw.png',
       name: 'Name 3',
       competition: 'Name of Competition',
       rank: 'Rank : 1st',
