248642f7accc5733a66ea080d7b2465e97f3d238
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2021-12-08 14:45:41 +0530

    Update Hero.js

--- config/content/Hero.js
+++ config/content/Hero.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 export default {
   dates: {
-    src: 'https://res.cloudinary.com/db4zymomi/image/upload/v1638908245/HackNITR/Group_105_rascmq.webp',
+    src: 'https://res.cloudinary.com/db4zymomi/image/upload/HackNITR/Group_105_rascmq.webp',
     alt: 'Image representing dates of the HackNITR Hackathon happening from 28-31 October 2021',
   },
   title: 'HackNITR 3.0',
@@ -18,7 +18,7 @@ export default {
     },
   },
   img: {
-    src: 'https://res.cloudinary.com/riteshsp2000/image/upload/project-oregano/assets/hwt5qvsejm6kjka0aon1.png',
+    src: 'https://res.cloudinary.com/db4zymomi/image/upload/v1638908198/HackNITR/Illustartion_qbdtpn.webp',
     alt: 'Howdy buddy! We are back with another awesome virtual hackathon, HackNITR',
   },
 };
