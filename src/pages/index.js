398dff2b8441fa3e4a399250b14ebd505bbbbcc6
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2022-10-16 14:16:29 +0530

    feat: install tailwind and config global css file (#162)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Home() {
-  return <div>HackNITR 4.0</div>;
+  return <h1 className='text-3xl font-bold underline text-white'>HackNITR 4.0</h1>;
 }
