57623263030f690e0c78285d7d56642027d5acb8
Ayush Shaw <61586233+ayushshaw90@users.noreply.github.com>
2022-11-06 17:36:08 +0530

    added section layout

--- src/components/carousel/ResponsiveCarousel.jsx
+++ src/components/carousel/ResponsiveCarousel.jsx
@@ -1,56 +1,61 @@
-import React from 'react'
+import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import Carousel from './Carousel';
 import CarouselMob from './CarouselMob';
+import { SectionLayout } from '../shared';
 
 export default function ResponsiveCarousel() {
-    let cards = [
-        {
-          id: 1,
-          heading: 'HOW TO PRESENT IN A HACKATHON',
-          name: 'Sivraj Rajshiv',
-          designation: 'MLH Lead',
-          date: '29 January',
-          label: 'LABEL',
-        },
-        {
-          id: 2,
-          heading: 'HOW TO PRESENT IN A EVENT',
-          name: 'Ayush Shaw',
-          designation: 'MLH Lead',
-          date: '29 January',
-          label: 'LABEL',
-        },
-        {
-          id: 3,
-          heading: 'Random text3',
-          name: 'Sivraj Rajshiv',
-          designation: 'MLH Lead',
-          date: '29 January',
-          label: 'LABEL',
-        },
-        {
-          id: 4,
-          heading: 'Random text4',
-          name: 'Sivraj Rajshiv',
-          designation: 'MLH Lead',
-          date: '29 January',
-          label: 'LABEL',
-        },
-      ];
-      const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-      
-      window.addEventListener('resize', ()=>{
-        setScreenWidth(window.innerWidth);
-      })
-  if(screenWidth >=640){
-    return(
-        <Carousel cards = {cards} focused={2} />
-    )
-  }else{
-    return(
-        <CarouselMob cards = {cards} focused={2} />
-    )
+  let cards = [
+    {
+      id: 1,
+      heading: 'HOW TO PRESENT IN A HACKATHON',
+      name: 'Sivraj Rajshiv',
+      designation: 'MLH Lead',
+      date: '29 January',
+      label: 'LABEL',
+    },
+    {
+      id: 2,
+      heading: 'HOW TO PRESENT IN A EVENT',
+      name: 'Ayush Shaw',
+      designation: 'MLH Lead',
+      date: '29 January',
+      label: 'LABEL',
+    },
+    {
+      id: 3,
+      heading: 'Random text3',
+      name: 'Sivraj Rajshiv',
+      designation: 'MLH Lead',
+      date: '29 January',
+      label: 'LABEL',
+    },
+    {
+      id: 4,
+      heading: 'Random text4',
+      name: 'Sivraj Rajshiv',
+      designation: 'MLH Lead',
+      date: '29 January',
+      label: 'LABEL',
+    },
+  ];
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  window.addEventListener('resize', () => {
+    setScreenWidth(window.innerWidth);
+  });
+  if (screenWidth >= 640) {
+    return (
+      <SectionLayout children={<Carousel cards={cards} focused={2} />}>
+        
+      </SectionLayout>
+    );
+  } else {
+    return (
+      <SectionLayout children={<CarouselMob cards={cards} focused={2} />}>
+        
+      </SectionLayout>
+    );
   }
 }
