dafb52122518254c6fcca3db514c149547821a69
SirSimon162 <anmolpattnayak2016@gmail.com>
2022-11-20 09:56:02 +0530

    fix:minor changes

--- src/components/shared/DiscordBtnMobile.jsx
+++ src/components/shared/DiscordBtnMobile.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import content from '../../../config/Hero';
+import content from '../../../config/hero';
 
 const Wrapper = styled.div`
   display: none;
