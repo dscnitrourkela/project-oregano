03210b8d83ed33cec3c26a79b05faa7a15f8d12d
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2022-12-20 16:28:19 +0530

    feat: add Prizes

--- config/content/Sponsors.js
+++ config/content/Sponsors.js
@@ -2,7 +2,7 @@
 export default {
   title: 'Our Awesome Sponsors',
   content:
-    'Our love for open source and drive to make an impact materialized as HackNITR 3.0. Backed by likes of participating students, we grew steadily to organize hackathons and support social initiatives. Today,we bring you the opportunity to collaborate with us on our next endeavour.',
+    'Our love for open source and drive to make an impact materialized as HackNITR 4.0. Backed by likes of participating students, we grew steadily to organize hackathons and support social initiatives. Today,we bring you the opportunity to collaborate with us on our next endeavour.',
   alpha: [
     {
       src: 'https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/sponsors/past_rv4xvj.png',
