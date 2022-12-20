f7ebb55a4de2aeebc44722a6b38ad04022f60837
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2022-12-21 01:29:21 +0530

    feat: add modal

--- config/content/Format.js
+++ config/content/Format.js
@@ -7,31 +7,43 @@ export default {
   eventList: [
     {
       eventTitle: '6th-8th January',
-      description: 'round 1 begins',
+      description: 'Round 1 begins',
       eventNumber: 1,
-      column: '5 / 8',
+      column: '5 / 9',
       row: '1/3',
+      className: 'first',
     },
     {
       eventTitle: '6th-8th January',
-      description: 'round 1 begins',
+      description: 'Round 1 begins',
       eventNumber: 1,
-      column: '10 / 13',
-      row: '3/5',
+      column: '10 / 14',
+      row: '2/4',
+      className: 'second',
     },
     {
       eventTitle: '6th-8th January',
-      description: 'round 1 begins',
+      description: 'Round 1 begins',
       eventNumber: 1,
-      column: '6 / 9',
+      column: '10 / 14',
       row: '5/7',
+      className: 'first',
     },
     {
       eventTitle: '6th-8th January',
-      description: 'round 1 begins',
+      description: 'Round 1 begins',
       eventNumber: 1,
-      column: '1 / 4',
-      row: '7/9',
+      column: '5 / 9',
+      row: '6/8',
+      className: 'second',
     },
   ],
+
+  registerNow: {
+    description:
+      'Come and hop on the ship to endless endeavour of technology or shit. Register till 6th',
+    column: '1/4',
+    row: '3/6',
+    link: 'https://hacknitr4.devfolio.co/ ',
+  },
 };
--- src/components/Format/style.jsx
+++ src/components/Format/style.jsx
@@ -1,14 +1,27 @@
+/* eslint-disable max-len */
 import styled from 'styled-components';
 import tw from 'twin.macro';
 
 export const Wrapper = styled.div`
+  grid-template-rows: repeat(7, minmax(0, 1fr));
+  grid-template-columns: repeat(13, minmax(0, 1fr));
+  background-image: url(https://res.cloudinary.com/dmutbjmoo/image/upload/v1671562171/Ellipse_55_l6ixzq.svg);
+  background-repeat: no-repeat;
+  background-position: 24% 45%;
+  background-size: 90% auto;
   ${tw`
-    grid  
-    md:grid-cols-12
-    py-10
-    w-full
+  grid  
+  py-10
+  w-full
   `}
-  grid-template-rows : repeat(8, minmax(0, 1fr))
+  @media (max-width: 720px) {
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+    grid-template-rows: repeat(5, minmax(0, 1fr));
+    gap: 24px;
+    background-image: url(https://res.cloudinary.com/dmutbjmoo/image/upload/v1671565643/Vector_gzlljh.svg);
+    background-repeat: no-repeat;
+    background-size: 500px 600px;
+  }
 `;
 
 export const Container = styled.div`
@@ -19,22 +32,34 @@ export const Container = styled.div`
 `;
 
 export const EventContainer = styled.div`
-  padding: 24px 16px;
+  padding: 32px 48px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 16px;
+  gap: 20px;
   border-radius: 12px;
   background-color: #202020;
+
+  @media (max-width: 720px) {
+    &.first {
+      grid-column: 1 / 4 !important;
+      grid-row: span 1 !important;
+    }
+
+    &.second {
+      grid-column: 2 / 5 !important;
+      grid-row: span 1 !important;
+    }
+  }
 
   .event-description {
     background-color: #fff49633;
     width: 100%;
-    border-radius: 200px;
+    border-radius: 44px 14px 14px 44px;
     position: relative;
     text-align: center;
-    padding: 6px 0;
+    padding: 8px 0;
   }
 
   .event-number {
@@ -43,7 +68,7 @@ export const EventContainer = styled.div`
     position: absolute;
     left: 0;
     top: 2px;
-    padding: 8px 4px;
+    padding: 12px 8px;
   }
 
   button {
@@ -51,4 +76,56 @@ export const EventContainer = styled.div`
     padding: 4px 8px;
     color: #fff;
   }
+
+  @media (max-width: 720px) {
+    &{
+      gap: 8px;
+      padding: 16px 12px;
+    }
+
+    p {
+      font-size: 12px;
+    }
+
+    h4{
+      font-size: 14px;
+    }
+`;
+
+export const RegisterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  align-items: flex-start;
+  justify-content: center;
+  
+  .title {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  button {
+    margin-bottom: 6px;
+  }
+
+  @media (max-width: 720px) {
+    &{
+      grid-column : 1/-1 !important;
+      grid-row: span 1 !important;
+      margin-bottom: 16px;
+      align-items: center;
+    }
+
+    p {
+      font-size: 12px;
+    }
+
+    h4{
+      font-size: 18px;
+    }
+
+    img{
+      height: 50px;
+    }
 `;
