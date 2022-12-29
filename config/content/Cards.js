f8e1329fa0cbe2dee54f5ebbe1d9a6c384216ce9
Ratnakar Sahoo <ratnakar3895@yahoo.com>
2022-12-30 01:11:59 +0530

    chore : youtube links update (#201)

--- config/content/Cards.js
+++ config/content/Cards.js
@@ -11,6 +11,7 @@ export default {
       Date: '2022-12-21',
       label: '6pm IST',
       img: 'https://res.cloudinary.com/dmutbjmoo/image/upload/v1671788870/HackNITR/Siddharth-Storyblok_ftnrtt.jpg',
+      vidLink: 'https://youtu.be/xYbmPh2Igbc',
     },
     {
       id: 1,
@@ -21,6 +22,7 @@ export default {
       Date: '2022-12-26',
       label: '6:30 pm IST',
       img: 'https://res.cloudinary.com/dmutbjmoo/image/upload/v1671788868/HackNITR/IMG-20200126-WA0014_e33aax.jpg',
+      vidLink: 'https://youtu.be/kY1v_bJq7HQ',
     },
     {
       id: 2,
@@ -31,6 +33,7 @@ export default {
       Date: '2022-12-28',
       label: '8:00 pm IST',
       img: 'https://res.cloudinary.com/dmutbjmoo/image/upload/v1671951110/HackNITR/20221111_171044_xxvgru.jpg',
+      vidLink: 'https://youtu.be/C9ADE6lDUjY',
     },
   ],
 };
