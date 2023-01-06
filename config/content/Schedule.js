2146881fe7b1681a55e94298c6c44061c470413a
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-01-06 19:59:35 +0530

    fix: workshop time

--- config/content/Schedule.js
+++ config/content/Schedule.js
@@ -1,32 +1,110 @@
 export default {
-    schedule: [
+  schedule: [
+    {
+      date: '06 Jan',
+      events: [
         {
-            date:'06 Jan',
-            events:[
-                {type:'session',eventName:'How to Build leaner applications using utility-as-a-service by Apyhub', img:'', speakerName:'Speaker Name', time:'07:00 PM'},
-                {type:'event',eventName:'Opening Ceremony', img:'', speakerName:'Speaker Name', time:'08:30 PM'},
-                {type:'event',eventName:'Hacking Hour Starts', img:'', speakerName:'Speaker Name', time:'09:00 PM'},
-                {type:'competition',eventName:'Game Night', img:'', speakerName:'Speaker Name', time:'10:00 PM'},
-            ],
+          type: 'session',
+          eventName: 'How to Build leaner applications using utility-as-a-service by Apyhub',
+          img: '',
+          speakerName: 'Speaker Name',
+          time: '07:00 PM',
         },
         {
-            date:'07 Jan',
-            events:[
-                {type:'session',eventName:'How to Build Scalable Decentralised Applications on Shardeum', img:'', speakerName:'Speaker Name', time:'11:00 AM'},
-                {type:'session',eventName:'Quine - Importance of Open Source', img:'', speakerName:'Speaker Name', time:'12:30 PM'},
-                {type:'competition',eventName:'MLH - Snyk CTF mini event by MLH', img:'', speakerName:'Speaker Name', time:'02:00 PM'},
-                {type:'session',eventName:'Tezos - A first smart contract on Tezos', img:'', speakerName:'Speaker Name', time:'04:00 PM'},
-                {type:'session',eventName:'The journey of an edtech company by Vivek Gupta', img:'', speakerName:'Speaker Name', time:'06:00 PM'},
-                {type:'competition',eventName:'MLH - Bob Ross mini event by MLH', img:'', speakerName:'Speaker Name', time:'09:00 PM'},
-                {type:'competition',eventName:'Game Night', img:'', speakerName:'Speaker Name', time:'10:00 PM'},
-            ],
+          type: 'event',
+          eventName: 'Opening Ceremony',
+          img: '',
+          speakerName: 'Speaker Name',
+          time: '08:30 PM',
         },
         {
-            date:'08 Jan',
-            events:[
-                {type:'event',eventName:'Submission Deadline', img:'', speakerName:'Speaker Name', time:'09:00 AM'},
-                {type:'event',eventName:'Hacking hour ends', img:'', speakerName:'Speaker Name', time:'10:00 AM'},
-            ],
-        }, 
-    ],      
+          type: 'event',
+          eventName: 'Hacking Hour Starts',
+          img: '',
+          speakerName: 'Speaker Name',
+          time: '09:00 PM',
+        },
+        {
+          type: 'competition',
+          eventName: 'Game Night',
+          img: '',
+          speakerName: 'Speaker Name',
+          time: '10:00 PM',
+        },
+      ],
+    },
+    {
+      date: '07 Jan',
+      events: [
+        {
+          type: 'session',
+          eventName: 'How to Build Scalable Decentralised Applications on Shardeum',
+          img: '',
+          speakerName: 'Speaker Name',
+          time: '11:00 AM',
+        },
+        {
+          type: 'session',
+          eventName: 'Quine - Importance of Open Source',
+          img: '',
+          speakerName: 'Speaker Name',
+          time: '12:30 PM',
+        },
+        {
+          type: 'competition',
+          eventName: 'MLH - Snyk CTF mini event by MLH',
+          img: '',
+          speakerName: 'Speaker Name',
+          time: '02:00 PM',
+        },
+        {
+          type: 'session',
+          eventName: 'The journey of an edtech company by Vivek Gupta',
+          img: '',
+          speakerName: 'Speaker Name',
+          time: '06:00 PM',
+        },
+        {
+          type: 'session',
+          eventName: 'Tezos - A first smart contract on Tezos',
+          img: '',
+          speakerName: 'Speaker Name',
+          time: '07:00 PM',
+        },
+        {
+          type: 'competition',
+          eventName: 'MLH - Bob Ross mini event by MLH',
+          img: '',
+          speakerName: 'Speaker Name',
+          time: '09:00 PM',
+        },
+        {
+          type: 'competition',
+          eventName: 'Game Night',
+          img: '',
+          speakerName: 'Speaker Name',
+          time: '10:00 PM',
+        },
+      ],
+    },
+    {
+      date: '08 Jan',
+      events: [
+        {
+          type: 'event',
+          eventName: 'Submission Deadline',
+          img: '',
+          speakerName: 'Speaker Name',
+          time: '09:00 AM',
+        },
+        {
+          type: 'event',
+          eventName: 'Hacking hour ends',
+          img: '',
+          speakerName: 'Speaker Name',
+          time: '10:00 AM',
+        },
+      ],
+    },
+  ],
 };
