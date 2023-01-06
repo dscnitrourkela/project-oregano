48a832daa233959fb2fcdae0a141c3745a543f42
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-01-06 18:18:26 +0530

    feat: add discord link

--- config/content/Hero.js
+++ config/content/Hero.js
@@ -17,11 +17,11 @@ export default {
   links: {
     discord: {
       icon: 'https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/Discord-Logo-Black_ratsjn.svg',
-      link: 'https://discord.com/invite/rxMB9aWbJD',
+      link: 'https://discord.com/invite/yQZ368XPrB',
     },
     discordMobile: {
       icon: 'https://res.cloudinary.com/ratnakar5938/image/upload/v1667584745/hn/Discord__software_-Logo_Wordmark-Black-Logo.wine_rhsgsh.png',
-      link: 'https://discord.com/invite/rxMB9aWbJD',
+      link: 'https://discord.com/invite/yQZ368XPrB',
     },
   },
 };
