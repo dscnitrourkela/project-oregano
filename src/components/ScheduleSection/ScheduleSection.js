69193825f1d5ea9c53abf4dae7c90929eb01c008
Jayesh <100998898+starkjay21@users.noreply.github.com>
2023-01-06 12:55:41 +0530

    feat : schedule section added

---
+++ src/components/ScheduleSection/ScheduleSection.js
@@ -0,0 +1,74 @@
+import React from "react";
+import schedule from "../../../config/content/Schedule";
+import { Body1, Body2, Caption, Heading2, Heading3, Heading4 } from "../shared";
+import { SectionLayout } from "../shared";
+import { GridContainer, DateContainer, EventContainer, EventDetailContainer, EventDetails, EventSpeaker, StarJediFont, EventCompletion, EventIndividualContainer, DottedLine } from "./styles";
+import styled from "styled-components";
+import tw from "twin.macro";
+const Caption1 = styled(Caption)`
+${tw`
+    text-[#FFD60A] 
+    capitalize
+`}
+`;
+
+const EventHeading = styled(Heading4)`
+width:100%;
+white-space: nowrap;
+overflow: hidden;
+text-overflow: ellipsis;
+`
+
+export default function ScheduleSection() {
+    return (
+        <>
+            <div className='w-full  mb-9'>
+                <Heading2 semibold>ROUND 1</Heading2>
+            </div>
+            <StarJediFont />
+            <SectionLayout>
+                <GridContainer>
+                    {
+                        schedule.schedule.map(({ date, events }) => (
+                            <>
+                                <DateContainer>
+                                    {date}
+                                </DateContainer>
+                                <EventContainer>
+                                    <DottedLine length={events.length} />
+                                    {
+                                        events.map(({ type, eventName, img, speakerName, time, completed }, idx) => (
+                                            <EventIndividualContainer key={idx}>
+                                                {/* <img src='https://res.cloudinary.com/dme9vltjf/image/upload/v1672810798/Schedule%20Section/Ellipse_1_xr37vx.png' /> */}
+                                                <EventCompletion completed={completed} />
+                                                <EventDetailContainer>
+                                                    <EventDetails>
+                                                        <Caption1 >{type}</Caption1>
+                                                        <EventHeading semibold>{eventName}</EventHeading>
+                                                        <EventSpeaker>
+                                                            {img ? (
+                                                                <>
+                                                                    <img src={img} />
+                                                                    <Body2 style={{fontSize:'12px'}}>{speakerName}</Body2>
+                                                                </>
+                                                            ) : (
+                                                                <div />
+                                                            )}
+                                                        </EventSpeaker>
+                                                    </EventDetails>
+
+                                                    <Body2 className="text-[#C7C7CC]">{time}</Body2>
+                                                </EventDetailContainer>
+                                            </EventIndividualContainer>
+                                        ))
+                                    }
+                                </EventContainer>
+                            </>
+                        ))
+                    }
+                </GridContainer>
+            </SectionLayout>
+
+        </>
+    );
+}
