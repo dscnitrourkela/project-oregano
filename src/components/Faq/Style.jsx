2b36dca2a9f6a715677ebbcf1ffdbda66c7685d3
RickDeb2004 <debanjanrick04@gmail.com>
2023-10-23 22:11:21 +0530

    fix: question alignment

--- src/components/Faq/Style.jsx
+++ src/components/Faq/Style.jsx
@@ -7,6 +7,10 @@ export const FAQContainer = styled.div`
   top: 6109px;
   left: 139px;
   gap: 32px;
+  display: flex; /* Center the heading */
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const FAQHeading = styled.div`
@@ -24,21 +28,29 @@ export const FAQCaption = styled.div`
   font-size: 24px;
   font-weight: 400;
   line-height: 36px;
-
+  text-align:center;
   color: #fff;
 `;
 
 export const FAQQuestionContainer = styled.div`
-  display: inline-flex;
+  width: auto;
+    height: 80px;
+display: inline-flex;
   flex-direction: column;
-  align-items: center;
+  
   gap: 32px;
+  
+    
+
+  
+  
 `;
 
 export const FAQQuestion = styled.div`
+ 
   display: flex;
   padding: 16px 36px;
-  align-items: center;
+  align-items: flex-start;
   gap: 404px;
   border-radius: 16px;
   border: 2px solid var(--neutral-3, #d9d9d9);
@@ -50,6 +62,8 @@ export const FAQQuestion = styled.div`
   line-height: 36px;
   width: 638px;
   cursor: pointer;
+  white-space: nowrap;
+
 `;
 
 export const FAQIcon = styled.div`
@@ -58,6 +72,7 @@ export const FAQIcon = styled.div`
   position: absolute;
   left: 12px;
   top: 12px;
+  cursor:pointer;
 `;
 
 export const FAQOpenIcon = styled.div`
@@ -72,4 +87,10 @@ export const FAQAnswer = styled.div`
   display: ${(props) => (props.isOpen ? 'block' : 'none')};
   margin-top: 16px;
   color: #fff;
+  width: 100%; 
+  position :relative;
+  padding: 20px;
+  text-align: center;
+   
+
 `;
--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-import { Body1, Body2, Heading1, Heading2, Heading3, Heading4 } from '../components';
+
 import FAQ from '../components/Faq/Faq';
 
-import { Body1, Body2, Heading1, Heading2, Heading3, Heading4, NavText, ButtonMeta, ButtonMetaLarge } from '../components';
+import {
+  Body1,
+  Body2,
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
+  NavText,
+  ButtonMeta,
+  ButtonMetaLarge
+} from '../components';
 
 
 export default function Playground() {
