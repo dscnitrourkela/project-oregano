e789f2473f4c415d7e05bbecda700de9dcc4854e
ishita-1305 <ishsri00@gmail.com>
2023-10-23 14:46:05 +0530

    new changes

--- config/content/Hero.js
+++ config/content/Hero.js
@@ -1,32 +1,9 @@
 /* eslint-disable max-len */
-import NITR from '../../images/NITR.png'
 export default {
   id: 'home',
   titleLine: { text: '' },
   tagline: {
     
     taglinedesc: "Hop on the bandwagon, and join one of India’s largest Student-run hackathons. HackNITR is back with version 5.0, which is now fully offline!",
-  },
-  Hackimg: {
-    src: 'https://res.cloudinary.com/ratnakar5938/image/upload/v1669992432/hn/Hack_n8mngx.png',
-    alt: 'Hack',
-  },
-  NITRimg: {
-    src: {NITR},
-    alt: 'NITR',
-  },
-  Fiveimg:{
-    src: {NITR},
-    alt: 'NITR',
-  },
-  links: {
-    discord: {
-      icon: 'https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/Discord-Logo-Black_ratsjn.svg',
-      link: 'https://discord.com/invite/yQZ368XPrB',
-    },
-    discordMobile: {
-      icon: 'https://res.cloudinary.com/ratnakar5938/image/upload/v1667584745/hn/Discord__software_-Logo_Wordmark-Black-Logo.wine_rhsgsh.png',
-      link: 'https://discord.com/invite/yQZ368XPrB',
-    },
   },
 };
