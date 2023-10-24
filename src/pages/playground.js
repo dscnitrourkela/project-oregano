1bef1498888da0b5e50e4f4c98c8e479b86ad0aa
Seventy7dot2 <22f3000994@ds.study.iitm.ac.in>
2023-10-24 08:30:10 +0530

    feat:timeline-extensions added

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { Body1, Body2, Heading1, Heading2, Heading3, Heading4 } from '../components';
 import Timeline from '../components/timeline/Timeline'
+import Extensionright from '../components/timeline/Extensionright'
+import Extensionboth from '../components/timeline/Extensionboth'
 export default function Playground() {
+  let event={
+    date:15,
+    month:"October",
+    title:"Registration Opens",
+    date_end:"15 dec"
+    }
   return (
     <div>
       <Heading1>Heading1</Heading1>
@@ -11,6 +19,8 @@ export default function Playground() {
       <Body1>Heading1</Body1>
       <Body2>Heading1</Body2>
       <Timeline></Timeline>
+      <Extensionright event={event}></Extensionright>
+      <Extensionboth></Extensionboth>
     </div>
   );
 }
