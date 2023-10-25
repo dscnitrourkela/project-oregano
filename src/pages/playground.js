c855c02fa35574962a98cdc08f63a2f28277543b
Seventy7dot2 <22f3000994@ds.study.iitm.ac.in>
2023-10-25 18:00:52 +0530

    Minor Changes

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import { Body1, Body2, Heading1, Heading2, Heading3, Heading4 } from '../components';
 import Timeline from '../components/timeline/Timeline'
 import { Body1, Body2, Heading1, Heading2, Heading3, Heading4, NavText, ButtonMeta, ButtonMetaLarge } from '../components';
 
