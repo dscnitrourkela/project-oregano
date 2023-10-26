ea1ab8549d3dfc56da721569b068a9d48147c18e
RickDeb2004 <debanjanrick04@gmail.com>
2023-10-26 11:20:24 +0530

    fix: again the position of icon

--- src/components/Faq/Style.jsx
+++ src/components/Faq/Style.jsx
@@ -147,7 +147,7 @@ export const FAQIcon = styled.div`
   position: absolute;
   //   left: 12px;
   font-size: 24px;
-  right: 200px;
+  right: 250px;
   position: absolute;
   tarnsform: translateY(-50%);
   transition: 0.3s;
