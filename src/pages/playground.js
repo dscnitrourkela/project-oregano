291aa47caee563cfabe7bd320b401233abf02e3e
RickDeb2004 <debanjanrick04@gmail.com>
2023-11-08 09:46:54 +0530

    fix: fixed faq component

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,19 +1,14 @@
 import React from 'react';
 
-
 import FAQ from '../components/Faq/Faq';
 
 import Timeline from '../components/timeline/Timeline';
 
-
 export default function Playground() {
   return (
     <div>
-
-      <FAQ />
-
       <Timeline />
-
+      <FAQ />
     </div>
   );
 }
