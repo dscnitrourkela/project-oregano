87e17d9029ffbb94ab052d74d94c0e6c2c30e667
praptippradhan <121177114+praptippradhan@users.noreply.github.com>
2023-11-08 19:30:12 +0530

    feat: add sponsor section (#261)
    * fix: styling changes in delta section
    
    * fix: styling changes in delta sponsors
    
    * fix: size of gamma and delta containers in mobile view
    
    * fix: prettier issues
    
    ---------
    
    Co-authored-by: Ashutosh Rath <ashutosh123rath@gmail.com>

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import SponsorSection from "../components/sponsor section/Sponsor";
 import FAQ from '../components/Faq/Faq';
-
 import Timeline from '../components/timeline/Timeline';
 
 export default function Playground() {
   return (
     <div>
+      <SponsorSection />
       <Timeline />
       <FAQ />
     </div>
