bb06a4dced6aa5ab42d411004fab10c060a38b8a
Prasad Patra <114677481+Seventy7dot2@users.noreply.github.com>
2023-11-08 09:24:51 +0530

    feat:added timeline (#247)
    * feat:added timeline
    
    * section:timeline added
    
    * feat:timeline-extensions added
    
    * feat:style components used
    
    * Delete src/.DS_Store
    
    * Delete .DS_Store
    
    * Delete src/components/.DS_Store
    
    * Minor Changes
    
    * feat:Mobile view added for Extensionboth
    
    * Added mobile view for ExtensionRight
    
    * Minor fix
    
    * feat:Mobile View added
    
    * feat:Mapping added
    
    * fix:Ellipse Overlapping h1
    
    * Delete timeline.css
    
    * fix:extra line on last node
    
    * fix:Typography added
    
    * minor:removed unrequired imports
    
    * fix:Improved Code Readability
    
    * feat:Optimised code
    
    * feat:Code Optimization
    
    * fix:UI for Mobile S
    
    * feat:Added padding to wrapper
    
    * fix:Allignment and padding in Mobile and Tablet view
    
    * fix: Removed unwanted attributes
    
    * minor:Renamed Desktop to DesktopView
    
    * fix:Removed extra div from BoxWrapper
    
    * fix:Rightline color issue and code optimization
    
    ---------
    
    Co-authored-by: Ashutosh Rath <ashutosh123rath@gmail.com>

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,30 +1,10 @@
 import React from 'react';
-import {
-  Body1,
-  Body2,
-  Heading1,
-  Heading2,
-  Heading3,
-  Heading4,
-  NavText,
-  ButtonMeta,
-  ButtonMetaLarge,
-} from '../components';
+import Timeline from '../components/timeline/Timeline';
 
 export default function Playground() {
   return (
     <div>
-      <Heading1>GDSC</Heading1>
-      <Heading1 className='italic'>GDSC</Heading1>
-      <Heading2>GDSC</Heading2>
-      <Heading2>GDSC</Heading2>
-      <Heading3>GDSC</Heading3>
-      <Heading4>GDSC</Heading4>
-      <NavText>GDSC</NavText>
-      <Body1>GDSC</Body1>
-      <Body2>GDSC</Body2>
-      <ButtonMeta>GDSC</ButtonMeta>
-      <ButtonMetaLarge>GDSC</ButtonMetaLarge>
+      <Timeline />
     </div>
   );
 }
