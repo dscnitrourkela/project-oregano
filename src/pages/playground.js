72bc5ccbc6554115d2d8aa94999b6ccb1ddee4fd
Sakshi Jaiswal <118760765+babitarit@users.noreply.github.com>
2023-11-08 20:09:34 +0530

    fix: Update playground.js

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import AboutUs from '../components/AboutUs/About';
 import FAQ from '../components/Faq/Faq';
-import Timeline from '../components/timeline/Timeline';
 export default function Playground() {
   return (
     <div>
       <AboutUs />
-      <Timeline />
       <FAQ />
     </div>
   );
