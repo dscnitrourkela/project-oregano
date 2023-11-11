552b6cd77f8fcf583f7751f14b307146119d5936
sakshi <ayushmanj138@gmail.com>
2023-11-11 17:05:58 +0530

    faq imported

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import AboutUs from '../components/AboutUs/About';
+import Faq from '../components/Faq/Faq';
 export default function Playground() {
   return (
     <div>
       <AboutUs />
+      <Faq />
   </div>
   );
 }
