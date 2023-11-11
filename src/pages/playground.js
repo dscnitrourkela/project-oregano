4fa003884119225668a234ca474864f04e9b3c71
Ashutosh Rath <ashutosh123rath@gmail.com>
2023-11-12 02:57:18 +0530

    Revert "Navigation (#267)"
    This reverts commit ab1b72373d01a87212edf92f75b683da9f60cd42.

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,17 +1,10 @@
-import React, { useState } from 'react';
-import NavBar  from '../components/marginals/Navbar/navbar';
-import { MenuContext } from '../components/marginals/Navbar/Menuontext';
+import React from 'react';
+import Hero from '../components/HeroSection/HeroSection';
 
 export default function Playground() {
-
-  const [menuOpen, setMenuOpen] = useState(false)
-  const toggleMenuOpen = () =>
-    menuOpen ? setMenuOpen(false) : setMenuOpen(true)
   return (
-    <>
-    <MenuContext.Provider value={{ menuOpen, toggleMenuOpen }}>
-      <NavBar />
-    </MenuContext.Provider>
-  </>
+    <div>
+      <Hero/>
+    </div>
   );
 }
