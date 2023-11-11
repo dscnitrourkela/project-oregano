ab1b72373d01a87212edf92f75b683da9f60cd42
riyapojage <133016157+riyapojage@users.noreply.github.com>
2023-11-12 02:56:39 +0530

    Navigation (#267)
    * color theme
    
    * feat/add navbar
    
    ---------
    
    Co-authored-by: Ashutosh Rath <ashutosh123rath@gmail.com>

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import Hero from '../components/HeroSection/HeroSection';
+import React, { useState } from 'react';
+import NavBar  from '../components/marginals/Navbar/navbar';
+import { MenuContext } from '../components/marginals/Navbar/Menuontext';
 
 export default function Playground() {
+
+  const [menuOpen, setMenuOpen] = useState(false)
+  const toggleMenuOpen = () =>
+    menuOpen ? setMenuOpen(false) : setMenuOpen(true)
   return (
-    <div>
-      <Hero/>
-    </div>
+    <>
+    <MenuContext.Provider value={{ menuOpen, toggleMenuOpen }}>
+      <NavBar />
+    </MenuContext.Provider>
+  </>
   );
 }
