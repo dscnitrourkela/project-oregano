2d8028fe05b247e9d63a148c2f67d073156392e6
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-12-11 17:56:49 +0530

    chore: update images

--- config/content/About.js
+++ config/content/About.js
@@ -2,11 +2,9 @@
 export default {
   desc: 'HackNITR 5.0 is a hackathon to encourage you to use your imagination and invention to develop cutting-edge technology-based solutions to challenges in the real world. It was put together by a large group of tech enthusiasts from Google Developer Student Club (GDSC) NIT Rourkela and OpenCode. The main goal is to grow our local community and give hackers an unforgettable experience. The plethora of fun and excitement is waiting for you from 27th Jan 2024.',
   carouselImages: [
-    'https://res.cloudinary.com/dafdencvh/image/upload/v1695910990/hacknitr5.0%20maerae%20component%20kae%20regarding%20image/image_hack1_v0w65x.png',
-    'https://res.cloudinary.com/dafdencvh/image/upload/v1695910980/hacknitr5.0%20maerae%20component%20kae%20regarding%20image/image_hack2_ndvw4p.png',
-    'https://res.cloudinary.com/dafdencvh/image/upload/v1695910973/hacknitr5.0%20maerae%20component%20kae%20regarding%20image/image_3_qpw8go.png',
-    'https://res.cloudinary.com/dafdencvh/image/upload/v1695910990/hacknitr5.0%20maerae%20component%20kae%20regarding%20image/image_hack1_v0w65x.png',
-    'https://res.cloudinary.com/dafdencvh/image/upload/v1695910980/hacknitr5.0%20maerae%20component%20kae%20regarding%20image/image_hack2_ndvw4p.png',
-    'https://res.cloudinary.com/dafdencvh/image/upload/v1695910973/hacknitr5.0%20maerae%20component%20kae%20regarding%20image/image_3_qpw8go.png',
+    'https://res.cloudinary.com/dhnkuonev/image/upload/v1702296582/pmtopml0tqukabudsrvp.jpg',
+    'https://res.cloudinary.com/dhnkuonev/image/upload/v1702296581/q3xgywx6x0pdqjymuyld.jpg',
+    'https://res.cloudinary.com/dhnkuonev/image/upload/v1702296579/ex0vix6wbrx7g5hlot2w.jpg',
+    'https://res.cloudinary.com/dhnkuonev/image/upload/v1702296987/uveuklce3owmbgbymozg.jpg',
   ],
 };
