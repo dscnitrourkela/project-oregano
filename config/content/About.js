f1be1b86217ba834c6686a80e458702ff0f49e24
Ashutosh Rath <ashutosh123rath@gmail.com>
2023-12-15 21:05:36 +0530

    chore: Update About.js (#283)

--- config/content/About.js
+++ config/content/About.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 export default {
-  desc: 'HackNITR 5.0 is a hackathon to encourage you to use your imagination and invention to develop cutting-edge technology-based solutions to challenges in the real world. It was put together by a large group of tech enthusiasts from Google Developer Student Club (GDSC) NIT Rourkela and OpenCode. The main goal is to grow our local community and give hackers an unforgettable experience. The plethora of fun and excitement is waiting for you from 27th Jan 2024.',
+  desc: 'HackNITR 5.0 is a hackathon to encourage you to use your imagination and invention to develop cutting-edge technology-based solutions to challenges in the real world. It was put together by a large group of tech enthusiasts from Google Developer Student Club (GDSC) NIT Rourkela and OpenCode. The main goal is to grow our local community and give hackers an unforgettable experience. The plethora of fun and excitement is waiting for you.',
   carouselImages: [
     'https://res.cloudinary.com/dhnkuonev/image/upload/v1702296582/pmtopml0tqukabudsrvp.jpg',
     'https://res.cloudinary.com/dhnkuonev/image/upload/v1702296581/q3xgywx6x0pdqjymuyld.jpg',
