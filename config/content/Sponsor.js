c25524464eced7f3e072190f144eeac51aad3e5e
Ashutosh Rath <ashutosh123rath@gmail.com>
2024-02-25 18:33:12 +0530

    chore: update sponsors (#313)

--- config/content/Sponsor.js
+++ config/content/Sponsor.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 export default {
   alpha: [
     {
@@ -6,23 +7,6 @@ export default {
       link: 'https://orkes.io/',
       alt: 'ORKES LOGO',
     },
-    // {
-    //   src:
-    //     'https://res.cloudinary.com/dzxgf75bh/image/upload/v1702202811/HackNitr/Sp
-    // onsor%20Section' +
-    //     '/jdorwxnipj0twbko2qod.png',
-    //   name: 'Major League Hacking',
-    //   link: 'https://mlh.io/',
-    //   alt: 'Major League Hacking',
-    // },
-    // {
-    //   src:
-    //     'https://res.cloudinary.com/dzxgf75bh/image' +
-    //     '/upload/v1702202812/HackNitr/Sponsor%20Section/zar2p2pouk5fvr8qsojj.png',
-    //   name: 'Polygon',
-    //   link: 'https://polygon.technology/',
-    //   alt: 'Polygon',
-    // },
   ],
   beta: [
     {
@@ -46,30 +30,6 @@ export default {
   ],
 
   platform: [
-    // {
-    //   src:
-    //     'https://res.cloudinary.com/dzxgf75bh/image' +
-    //     '/upload/v1702202813/HackNitr/Sponsor%20Section/jsvurmvvk3nrr4lmncy9.png',
-    //   name: 'Quine',
-    //   link: 'https://quine.sh/',
-    //   alt: 'Quine',
-    // },
-    // {
-    //   src:
-    //     'https://res.cloudinary.com/dzxgf75bh/image' +
-    //     '/upload/v1702202818/HackNitr/Sponsor%20Section/r2arfrrqfbwwtre4cnzm.png',
-    //   name: 'Solana',
-    //   link: 'https://solana.com',
-    //   alt: 'Solana',
-    // },
-    // {
-    //   src:
-    //     'https://res.cloudinary.com/dzxgf75bh/image' +
-    //     '/upload/v1702202820/HackNitr/Sponsor%20Section/en4bf0uiglauk6grmywe.png',
-    //   name: 'Filecoin',
-    //   link: 'https://filecoin.io',
-    //   alt: 'Filecoin',
-    // },
     {
       src:
         'https://res.cloudinary.com/dzxgf75bh/image' +
@@ -77,6 +37,8 @@ export default {
       name: 'Devfolio',
       link: 'https://devfolio.co/',
       alt: 'DEVFOLIO LOGO',
+      width: '200px',
+      height: '200px',
     },
     {
       src:
@@ -86,23 +48,51 @@ export default {
       link: 'https://polygon.technology/',
       alt: 'POLYGON LOGO',
       // width: '20%',
+      width: '200px',
+      height: '200px',
     },
-    // {
-    //   src:
-    //     'https://res.cloudinary.com/dzxgf75bh/image' +
-    //     '/upload/v1702202879/HackNitr/Sponsor%20Section/w9kndnmoqjxt5i2tq71t.png',
-    //   name: 'Shardeum',
-    //   link: 'https://shardeum.org/',
-    //   alt: 'Shardeum',
-    // },
-    // {
-    //   src:
-    //     'https://res.cloudinary.com/dzxgf75bh/image' +
-    //     '/upload/v1702202885/HackNitr/Sponsor%20Section/va0bxpx2de0le1zfjwex.png',
-    //   name: 'Digitalocean',
-    //   link: 'https://digitalocean.com/',
-    //   alt: 'Digitalocean',
-    // },
+    {
+      src: 'https://res.cloudinary.com/dkpdi1wek/image/upload/v1708786350/download__2_-removebg-preview_1_dscm20.png',
+      name: 'MathWorks',
+      link: 'https://in.mathworks.com/',
+      alt: 'MathWorks',
+      width: '200px',
+      height: '200px',
+    },
+
+    {
+      src: 'https://res.cloudinary.com/dkpdi1wek/image/upload/v1708786569/download__3_-removebg-preview_otfa7p.png',
+      name: 'mongoDB',
+      link: 'https://www.mongodb.com/',
+      alt: 'mongoDB',
+      width: '200px',
+      height: '200px',
+    },
+
+    {
+      src: 'https://res.cloudinary.com/dkpdi1wek/image/upload/v1708786739/download__5_-removebg-preview_zslel5.png',
+      name: 'GoDaddy Registry',
+      link: 'https://in.godaddy.com/',
+      alt: ' GoDaddy',
+      width: '200px',
+      height: '200px',
+    },
+    {
+      src: 'https://res.cloudinary.com/dkpdi1wek/image/upload/v1708787057/download_6_sop5ix.png',
+      name: 'auth0',
+      link: 'https://auth0.com/',
+      alt: 'Auth0',
+      width: '200px',
+      height: '200px',
+    },
+    {
+      src: 'https://res.cloudinary.com/dkpdi1wek/image/upload/v1708830042/download__7_-removebg-preview_qcloyx.png',
+      name: 'flow',
+      link: 'https://www.onflow.org/',
+      alt: 'flow',
+      width: '200px',
+      height: '200px',
+    },
   ],
   gamma: [
     {
@@ -153,42 +143,6 @@ export default {
       link: 'https://replit.com',
       alt: 'Replit',
     },
-    //   {
-    //     src:
-    //       'https://res.cloudinary.com/dzxgf75bh/image' +
-    //       '/upload/v1702202900/HackNitr/Sponsor%20Section/tatgslbybvbpqdydhjsn.png',
-    //     name: 'Major League Hacking',
-    //     link: 'https://mlh.io/',
-    //     alt: 'Major League Hacking',
-    //     width: '42%',
-    //   },
-    //   {
-    //     src:
-    //       'https://res.cloudinary.com/dzxgf75bh/image' +
-    //       '/upload/v1702202903/HackNitr/Sponsor%20Section/xpipftsfivsn4l1rhxot.png',
-    //     name: 'Polygon',
-    //     link: 'https://polygon.technology/',
-    //     alt: 'Polygon',
-    //     width: '42%',
-    //   },
-    //   {
-    //     src:
-    //       'https://res.cloudinary.com/dzxgf75bh/image' +
-    //       '/upload/v1702202904/HackNitr/Sponsor%20Section/pa0b377dp24u2a9nlwvl.png',
-    //     name: 'Wolfram',
-    //     link: 'https://www.wolfram.com/',
-    //     alt: 'Wolfram',
-    //     width: '42%',
-    //   },
-    //   {
-    //     src:
-    //       'https://res.cloudinary.com/dzxgf75bh/image' +
-    //       '/upload/v1702202908/HackNitr/Sponsor%20Section/d1d4i1o9sftwo3xbbl6s.png',
-    //     name: 'Twilio',
-    //     link: 'https://www.twilio.com/',
-    //     alt: 'Twilio',
-    //     width: '42%',
-    //   },
   ],
   deltaSecond: [
     {
