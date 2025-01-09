870ad9c626d26506bb65fe09cee11e327a09f49c
ayush <ayushatschool5@gmail.com>
2025-01-09 23:37:56 +0530

    fix: minor fixes

--- src/components/shared/typography/Headings.jsx
+++ src/components/shared/typography/Headings.jsx
@@ -9,11 +9,11 @@ export const H1 = styled.h1`
   -webkit-text-fill-color: transparent;
 `;
 
-export const H2 = styled.h3`
+export const H2 = styled.h2`
   ${tw`text-2xl font-satoshi font-semibold leading-snug tracking-normal sm:text-3xl md:text-4xl`}
 `;
 
-export const H3 = styled.h4`
+export const H3 = styled.h3`
   ${tw`text-xl font-prompt font-medium leading-snug tracking-normal sm:text-2xl md:text-3xl`}
 `;
 
